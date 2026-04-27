d8ec08ad519bd36417f39eed01ae93e634ae344b
Ivanfoya09 <ivanfoya09@gmai.com>
2026-04-27 15:43:58 +0300

    іфмуц

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = Flask(__name__)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
