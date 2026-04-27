752812664c7a3798571408b08044871257942761
Ivanfoya09 <ivanfoya09@gmai.com>
2026-04-27 17:40:59 +0300

    save

--- main.py
+++ main.py
@@ -210,26 +210,27 @@ with app.app_context():
     if not Menu.query.first():
         items = [
             Menu(name="Піца Маргарита", price=180, picture="images/pizza.jpg", category="pizza"),
-            Menu(name="Піца Пепероні", price=210, picture="images/pizza.jpg", category="pizza"),
-            Menu(name="Піца 4 сири", price=230, picture="images/pizza.jpg", category="pizza"),
-            Menu(name="Класичний бургер", price=150, picture="images/burger.jpg", category="burger"),
-            Menu(name="Чізбургер", price=170, picture="images/burger.jpg", category="burger"),
-            Menu(name="Дабл бургер", price=220, picture="images/burger.jpg", category="burger"),
+            Menu(name="Піца Пепероні", price=210, picture="images/pizza.jpg", category="pizza1"),
+            Menu(name="Піца 4 сири", price=230, picture="images/pizza.jpg", category="pizza2"),
 
-            Menu(name="Салат Цезар", price=120, picture="images/salad.jpg", category="salad"),
+            Menu(name="Класичний бургер", price=150, picture="images/burger.jpg", category="burger"),
+            Menu(name="Чізбургер", price=170, picture="images/burger.jpg", category="burger1"),
+            Menu(name="Дабл бургер", price=220, picture="images/burger.jpg", category="burger2"),
+
+            Menu(name="Салат Цезар", price=120, picture="images/salad.jpg", category="salad1"),
             Menu(name="Грецький салат", price=110, picture="images/salad.jpg", category="salad"),
 
-            Menu(name="Паста Карбонара", price=190, picture="images/pasta.jpg", category="pasta"),
+            Menu(name="Паста Карбонара", price=190, picture="images/pasta.jpg", category="pasta1"),
             Menu(name="Паста Болоньєзе", price=200, picture="images/pasta.jpg", category="pasta"),
 
             Menu(name="Борщ", price=90, picture="images/soup.jpg", category="soup"),
-            Menu(name="Крем-суп грибний", price=100, picture="images/soup.jpg", category="soup"),
+            Menu(name="Крем-суп грибний", price=100, picture="images/soup.jpg", category="soup1"),
 
             Menu(name="Картопля фрі", price=80, picture="images/fries.jpg", category="snack"),
-            Menu(name="Нагетси", price=120, picture="images/nuggets.jpg", category="snack"),
+            Menu(name="Нагетси", price=120, picture="images/nuggets.jpg", category="snack1"),
 
-            Menu(name="Кола", price=50, picture="images/drink.jpg", category="drink"),
-            Menu(name="Сік апельсиновий", price=60, picture="images/drink.jpg", category="drink"),
+            Menu(name="Кола", price=50, picture="images/drink.jpg", category="drink1"),
+            Menu(name="Сік апельсиновий", price=60, picture="images/drink.jpg", category="drink2"),
             Menu(name="Вода", price=30, picture="images/drink.jpg", category="drink"),
         ]
 
